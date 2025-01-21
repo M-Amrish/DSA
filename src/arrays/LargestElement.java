fa16c1d79434269277fa478b868a69ba1a65bb45
Amrish <amrish.m004@gmail.com>
2025-01-21 23:55:20 +0530

    Largest Element in Array

---
+++ src/arrays/LargestElement.java
@@ -0,0 +1,27 @@
+package arrays;
+
+/*
+Largest Element in Array
+
+Input: arr[] = [1, 8, 7, 56, 90]
+Output: 90
+ */
+public class LargestElement {
+    public static int largest(int[] arr) {
+
+        // brute force
+        // sort the array and check at last postion
+        //TC - N*Log(N) & SC - 1
+
+        int var = -1;
+
+        for(int i=0 ; i<arr.length; i++){
+
+            if(arr[i] > var){
+                var = arr[i];
+            }
+        }
+
+        return var;
+    }
+}
