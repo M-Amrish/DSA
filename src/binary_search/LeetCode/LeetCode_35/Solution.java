3fa08c5f399b3ee2ec8b7c1340c8f31675514641
Amrish <amrish.m004@gmail.com>
2025-02-17 09:50:28 +0530

    class Solution {

---
+++ src/binary_search/LeetCode/LeetCode_35/Solution.java
@@ -0,0 +1,22 @@
+package binary_search.LeetCode.LeetCode_35;
+
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+
+        int s=0, e=nums.length-1;
+
+        while(s<=e){
+
+            int m = (s+e)/2;
+            if(nums[m] == target){
+                return m;
+            }else if(nums[m] > target){
+                e = m-1;
+            }else{
+                s = m+1;
+            }
+        }
+        return s;
+    }
+}
